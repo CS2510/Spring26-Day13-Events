c6b96223d43322f25c8645e4d94d7c44bc1ddee7
bricksphd <bricks@unomaha.edu>
2026-03-04 07:01:32 -0600

    Setup for Day 14

--- gamePlatform/PlatformGameObject.js
+++ gamePlatform/PlatformGameObject.js
@@ -3,5 +3,7 @@ class PlatformGameObject extends GameObject{
   constructor(){
     super("PlatformGameObject")
     this.addComponent(new Polygon(), {points: [new Vector2(-100, -10), new Vector2(100, -10), new Vector2(100, 10), new Vector2(-100, 10)], fillStyle: "gray"})
+    this.addComponent(new Collider())
   }
 }
+
